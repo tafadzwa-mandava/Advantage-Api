a1b47c37549f05bdaf64e5eff2290abc67f636c0
Bunchies <t.mandava@outlook.com>
2019-04-04 23:54:46 +0200

    Debugging ApiContext class

--- Models/ApiContext.cs
+++ Models/ApiContext.cs
@@ -4,7 +4,7 @@ namespace Advantage.API.Models
 {
     public class ApiContext : DbContext
     {
-        public ApiContext(DbContextOptions<ApiContext> options : base(options)) 
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
         {
         }
 
